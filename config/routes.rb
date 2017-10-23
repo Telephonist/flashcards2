d2b0974d3575b3fdd37e137f2a541a48171f810d
Telephonist <chervonobab@gmail.xom>
2017-10-23 17:47:27 +0300

    3_task

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'cards/index'
 
   root 'welcome#index'
-  resources :cards, only: :index
+  resources :cards
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
