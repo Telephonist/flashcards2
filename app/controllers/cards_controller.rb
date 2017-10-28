b80a7cc934ddf9242bbd019c49b6f01b2fbc90bf
Telephonist <chervonobab@gmail.xom>
2017-10-28 20:55:14 +0300

    4_task

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,16 +3,19 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
   def show
     @card = Card.find(params[:id])
   end
+
   def new
     @card = Card.new
   end
+
   def edit
     @card = Card.find(params[:id])
-
   end
+
   def create
     @card = Card.new(card_params)
     @card.save
@@ -22,6 +25,7 @@ class CardsController < ApplicationController
       render 'new'
     end
   end
+
   def update
     @card = Card.find(params[:id])
 
@@ -31,13 +35,16 @@ class CardsController < ApplicationController
       render 'edit'
     end
   end
+
   def destroy
     @card = Card.find(params[:id])
     @card.destroy
     redirect_to cards_path
   end
+
   private
-    def card_params
-      params.require(:card).permit(:original_text, :translated_text, :review_date)
-    end
+
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text, :review_date)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+
+url = 'http://1000mostcommonwords.com/1000-most-common-german-words/'
+
+page = Nokogiri::HTML(open(url))
+
+dictionary = page.css('#post-188 > div > table > tbody > tr')
+
+dictionary.each do |t|
+  Card.create(original_text: t.css('td')[1].text, translated_text: t.css('td')[2].text, review_date: 3.days.from_now)
+end
